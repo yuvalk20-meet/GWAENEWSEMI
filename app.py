dc2b8838508df68aa32a940dc2a7c5dfcd970d2e
myname21-meet <myname21@meet.mit.edu>
2020-08-04 10:54:40 -0400

    css doesnt work

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ app = Flask(__name__)
 def home():
 	return render_template("index.html")
 
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/insta')
+def insta():
+	return render_template("services.html")
+
+@app.route('/change')
+def cases():
+	return render_template("cases.html")
+
+@app.route('/challenge')
+def challenge():
+	return render_template("blog.html")
+
+
+
 if __name__ == '__main__':
    app.run(debug = True)
 
