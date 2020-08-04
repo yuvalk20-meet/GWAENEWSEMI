498b87157f325fa73522f509da46109776267106
katrina20-meet <40925213+katrina20-meet@users.noreply.github.com>
2020-08-04 15:24:50 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from database import *
+
 from flask import Flask, render_template,url_for,request,redirect
-from flask import session as login_session
-import requests, json , flask , Flask
+
 
 app = Flask(__name__)
 
@@ -9,7 +8,5 @@ app = Flask(__name__)
 def home():
 	return render_template("index.html")
 
-
 if __name__ == '__main__':
    app.run(debug = True)
-
