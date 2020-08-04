8d33e0333a1dbc7406e5d98f0b30ef64ab138c54
myname21-meet <myname21@meet.mit.edu>
2020-08-04 17:01:56 +0300

    hi

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from flask import render_template
-=======
-
 from flask import Flask, render_template,url_for,request,redirect
->>>>>>> 498b87157f325fa73522f509da46109776267106
 
 
 app = Flask(__name__)
@@ -12,11 +7,6 @@ app = Flask(__name__)
 def home():
 	return render_template("index.html")
 
-<<<<<<< HEAD
-	if __name__ == "__main__":        
-    app.run()
-
-=======
 if __name__ == '__main__':
    app.run(debug = True)
->>>>>>> 498b87157f325fa73522f509da46109776267106
+
