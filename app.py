e5332b59a4f0dc4c5295e2867b72a11a35f49078
unknown <ward20-meet>
2020-08-04 15:25:54 +0300

    fonts

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from database import *
-from flask import Flask, render_template,url_for,request,redirect
-from flask import session as login_session
-import requests, json , flask , Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template("index.html")
+
+	if __name__ == "__main__":        
+    app.run()
 
